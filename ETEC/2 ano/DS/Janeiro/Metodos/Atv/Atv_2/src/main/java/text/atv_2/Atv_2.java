7fdb009028ae50a7cffb772460313a02ea634f6a
Endrigogustavo <endrigogustavo2006@gmail.com>
2023-02-16 21:52:25 -0300

    o

---
+++ ETEC/2 ano/DS/Janeiro/Metodos/Atv/Atv_2/src/main/java/text/atv_2/Atv_2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package text.atv_2;
+
+import javax.swing.JOptionPane;
+public class Atv_2 {
+   
+    public static void main(String[] args) {
+      float num1, num2;
+      Diferença();
+      num1 = 
+     
+    }
+    
+    static void Diferença()
+    {
+    JOptionPane.showMessageDialog(null, "Digite o Primeiro numero");
+    JOptionPane.showMessageDialog(null, "Digite o segundo numero");
+    }
+}
