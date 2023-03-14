b8c5e0ba4141586505abfa7e5d560883abca3257
/__Endrigo__ <113954684+Endrigogustavo@users.noreply.github.com>
2023-03-14 14:13:30 -0300

    Update MainActivity.kt

--- ETEC/2 ano/PAM/janeiro/Atv_01-03/app/src/main/java/com/example/app/MainActivity.kt
+++ ETEC/2 ano/PAM/janeiro/Atv_01-03/app/src/main/java/com/example/app/MainActivity.kt
@@ -6,17 +6,31 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val edtnome:EditText = findViewById(R.id.edtnome)
+        val end :EditText = findViewById(R.id.end)
+        val bai :EditText = findViewById(R.id.bai)
+        val cep :EditText = findViewById(R.id.cep)
+        val cid :EditText = findViewById(R.id.cid)
+        val est :EditText = findViewById(R.id.est)
         val btncadastar:Button = findViewById(R.id.cadastrar)
         btncadastar.setOnClickListener{
-            val toast = Toast.makeText(this, edtnome.text.toString(), Toast.LENGTH_SHORT)
+
+            val toast= Toast.makeText(this, end.text.toString()
+                    +"\n" + bai.text.toString()
+                    +"\n" + cep.text.toString()
+                    +"\n" + cid.text.toString()
+                    +"\n" + est.text.toString()
+                    , Toast.LENGTH_SHORT)
             toast.show()
         }
+
     }
 }
+
 
